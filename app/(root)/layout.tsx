43e049c039f126c1f6c49ed76f4b117553edc013
erzeeshankhan <er.zeeshank@gmail.com>
2025-03-17 23:41:25 +0530

    Header component addded

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Sidebar from "@/components/Sidebar";
-import MobileNavigation from "@/components/MobileNavigation";
-import Header from "@/components/Header";
+
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export const dynamic = "force-dynamic";
 
