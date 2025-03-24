5ce7443eb2e36c104ab5d3a38494e0f6e37c7910
erzeeshankhan <er.zeeshank@gmail.com>
2025-03-24 18:15:59 +0530

    file upload logic

--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -1,12 +1,25 @@
 "use server";
 
 import { UploadFileProps } from "@/types";
+import { createAdminClient } from "../appwrite";
 
+// Handle error function
+const handleError = (error: unknown, message: string) => {
+    console.error(message, error);
+    throw error;
+}
+// Upload file function
 export const uploadFile = async ({
     file,
     ownerId,
     accountId,
     path,
 }: UploadFileProps) => {
+    const { storage, databases } = await createAdminClient();
 
+    try {
+        const 
+    } catch (error) {
+        handleError(error, "Failed to upload file");
+    }
 }
